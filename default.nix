e84f8866f92d4cac0add25a95fa6cef593964a5f
Jacquin Mininger <jacquin.mininger@gmail.com>
2021-06-03 15:31:09 -0400

    fix merge problems

--- default.nix
+++ default.nix
@@ -1,15 +1,13 @@
-{ pactRef ? "ace18a2489bf86595af09316bd1c09794c326b77"
-, pactSha ? "13cg8nz8lc839xnfp31yj8ihra5qpgfz9g4lzqmgcfk4kmxcvlxi"
-, kpkgs ? import ./dep/kpkgs {}
-}:
-
+{ kpkgs ? import ./dep/kpkgs {}}:
 let
-pactSrc = builtins.fetchTarball {
-  url = "https://github.com/kadena-io/pact/archive/${pactRef}.tar.gz";
-  sha256 = pactSha;
-};
+  pactSrc = kpkgs.pkgs.fetchFromGitHub {
+    owner = "kadena-io";
+    repo = "pact";
+    rev = "4971ab6078b75eb612d83d56f1e7cd139a5a2ba8";
+    sha256 = "1jx1hd596r5rrx96r2v2xds6pjjmi4lfk7xm14f3gkx2gmavgyr3";
+  };
 
-signingProject = kpkgs.rp.project ({ pkgs, hackGet, ... }: with pkgs.haskell.lib; {
+  signingProject = kpkgs.rp.project ({ pkgs, hackGet, ... }: with pkgs.haskell.lib; {
     name = "kadena-signing-api";
     overrides = self: super: {
       pact = dontCheck ( addBuildDepend (self.callCabal2nix "pact" pactSrc {}) pkgs.z3);
